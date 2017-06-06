86d4c10c64a8f95864c4fe3eb224697918e6f684
Lanqing Huang <lqhuang@outlook.com>
2017-06-06 10:53:11 +0800

    Add batch normalization

--- deepheart/model.py
+++ deepheart/model.py
@@ -47,32 +47,34 @@ class CNN:
         with tf.name_scope('input'):
             X = tf.placeholder(tf.float32, [None, self.d_input], name='X')
             y = tf.placeholder(tf.float32, [None, self.nclasses], name='y')
+            is_train = tf.placeholder(tf.bool, name='phase_train')
             do_drop = tf.placeholder(tf.float32, name='drop')
 
-        with tf.name_scope('weights'):
-            weights = {
-                'wc1': tf.Variable(tf.random_normal([5, 1, 1, 32]), name='wc1'),
-                'wc2': tf.Variable(tf.random_normal([5, 1, 32, 64]), name='wc2'),
-                # 2 Max pools have taken original 10612 signal down to
-                # 5306 --> 2653. Each max pool has a ksize=2.
-                # 'wd1': tf.Variable(tf.random_normal([2653 * 1 * 64, 1024])),
-                'wd1': tf.Variable(tf.random_normal([int(self.d_input / 4) * 1 * 64, 1024]), name='wd1'),
-                'out': tf.Variable(tf.random_normal([1024, self.nclasses]), name='outW')
-            }
-        with tf.name_scope('biases'):
-            biases = {
-                'bc1': tf.Variable(tf.random_normal([32]), name='bc1'),
-                'bc2': tf.Variable(tf.random_normal([64]), name='bc2'),
-                'bd1': tf.Variable(tf.random_normal([1024]), name='bd1'),
-                'out': tf.Variable(tf.random_normal([self.nclasses]), name='outB')
-            }
+        # with tf.name_scope('weights'):
+        weights = {
+            'wc1': tf.Variable(tf.random_normal([5, 1, 1, 32]), name='wc1'),
+            'wc2': tf.Variable(tf.random_normal([5, 1, 32, 64]), name='wc2'),
+            # 2 Max pools have taken original 10612 signal down to
+            # 5306 --> 2653. Each max pool has a ksize=2.
+            # 'wd1': tf.Variable(tf.random_normal([2653 * 1 * 64, 1024])),
+            'wd1': tf.Variable(tf.random_normal([int(self.d_input / 4) * 1 * 64, 1024]), name='wd1'),
+            'out': tf.Variable(tf.random_normal([1024, self.nclasses]), name='outW')
+        }
+        # with tf.name_scope('biases'):
+        biases = {
+            'bc1': tf.Variable(tf.random_normal([32]), name='bc1'),
+            'bc2': tf.Variable(tf.random_normal([64]), name='bc2'),
+            'bd1': tf.Variable(tf.random_normal([1024]), name='bd1'),
+            'out': tf.Variable(tf.random_normal([self.nclasses]), name='outB')
+        }
 
         with tf.name_scope('pred'):
-            pred = self.model1D(X, weights, biases, do_drop)
+            pred = self.model1D(X, weights, biases, do_drop, is_train)
 
         with tf.name_scope('cost'):
             cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
             optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(cost)
+            tf.summary.scalar('cost', cost)
 
         dim = tf.shape(y)[0]
 
@@ -104,6 +106,7 @@ class CNN:
 
             merged = tf.summary.merge_all()
             train_writer = tf.summary.FileWriter(os.path.join(self.base_dir, 'train'), sess.graph)
+            test_writer = tf.summary.FileWriter(os.path.join(self.base_dir, 'test'))
 
             for epoch in range(self.epochs):
                 avg_cost = 0
@@ -112,19 +115,23 @@ class CNN:
                     summary, _, c = sess.run([merged, optimizer, cost],
                                              feed_dict={X: batch_x,
                                                         y: batch_y,
-                                                        do_drop: self.dropout})
-                    train_writer.add_summary(summary, epoch*batch)
+                                                        do_drop: self.dropout,
+                                                        is_train: True})
+                    step = epoch * self.nbatches + batch
+                    train_writer.add_summary(summary, step)
                     avg_cost += c
                 avg_cost /= float(self.nbatches)
                 print('Epoch %s\tcost %s' % (epoch, avg_cost))
 
                 if epoch % 10 == 0:
-                    acc, sens, spec = sess.run([score, sensitivity, specificity],
-                                                feed_dict={X: self.pcg.test.X,
-                                                           y: self.pcg.test.y,
-                                                           do_drop: 1.})
+                    summary, acc, sens, spec = sess.run([merged, score, sensitivity, specificity],
+                                                        feed_dict={X: self.pcg.test.X,
+                                                                   y: self.pcg.test.y,
+                                                                   do_drop: 1.,
+                                                                   is_train: False})
                     print('Score %s\tSensitivity %s\tSpecificity %s' % (acc, sens, spec))
-
+                    step = epoch * self.nbatches + batch
+                    test_writer.add_summary(summary, step)
                     saver.save(sess, self.__get_output_name())
                     print('Epoch written')
 
@@ -133,6 +140,38 @@ class CNN:
         time_str = "-%s" % (now.date())  # now.hour, now.minute, now.second)
         model_path = os.path.join(self.base_dir, self.model_name + time_str + '.tnfl')
         return model_path
+
+    @staticmethod
+    def batch_norm(x, n_out, phase_train):
+        """
+        Batch normalization on convolutional maps.
+        Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+        Args:
+            x:           Tensor, 4D BHWD input maps
+            n_out:       integer, depth of input maps
+            phase_train: boolean tf.Varialbe, true indicates training phase
+            scope:       string, variable scope
+        Return:
+            normed:      batch-normalized maps
+        """
+        with tf.variable_scope('bn'):
+            beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+                                           name='beta', trainable=True)
+            gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+                                            name='gamma', trainable=True)
+            batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+            ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+            def mean_var_with_update():
+                ema_apply_op = ema.apply([batch_mean, batch_var])
+                with tf.control_dependencies([ema_apply_op]):
+                    return tf.identity(batch_mean), tf.identity(batch_var)
+
+            mean, var = tf.cond(phase_train,
+                                mean_var_with_update,
+                                lambda: (ema.average(batch_mean), ema.average(batch_var)))
+            normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+        return normed
 
     def conv2d(self, x, w, b, strides=1):
         """
@@ -157,12 +196,11 @@ class CNN:
 
         """
 
-
         x = tf.nn.conv2d(x, w, strides=[1, strides, strides, 1], padding="SAME")
         x = tf.nn.bias_add(x, b)
         return tf.nn.relu(x)
 
-    def model1D(self, x, weights, biases, dropout):
+    def model1D(self, x, weights, biases, dropout, is_train):
         """
         A Wrapper to chain several TensorFlow convolutional units together. This 1D model
         ultimately calls TensorFlow's conv2d, mapping a 1D feature vector to a collapsed
@@ -196,11 +234,13 @@ class CNN:
         with tf.name_scope('conv1'):
             conv1 = self.conv2d(x, weights['wc1'], biases['bc1'])
             conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding='SAME')
+            conv1 = self.batch_norm(conv1, 32, is_train)
             conv1 = tf.nn.relu(conv1)
 
         with tf.name_scope('conv2'):
             conv2 = self.conv2d(conv1, weights['wc2'], biases['bc2'])
             conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding="SAME")
+            conv2 = self.batch_norm(conv2, 64, is_train)
             conv2 = tf.nn.relu(conv2)
 
         with tf.name_scope('fullyConnected'):
